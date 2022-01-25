b7f2893dbd1e24891c5e80eec2aaa1cd5ea80d24
mingw4 <mingw4@illinois.edu>
2022-01-24 23:24:38 -0600

    Tests Finished.

--- test/com/example/TicTacToeBoardTest.java
+++ test/com/example/TicTacToeBoardTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -7,15 +8,73 @@ public class TicTacToeBoardTest {
   /*
    * Testing strategy
    *
-   * Partitions evaluate() outcomes as follows:
-   * UnreachableState, Nowins, Xwins, Owins, and IllegalArgumentException
+   * Partitions evaluate() inputs as follows:
+   * InvalidBoard_IllegalArgumentException, InvalidBoard, ValidBoardNowinner, ValidBoardXwins, and ValidBoardOwins.
    */
 
+  @Test (expected = IllegalArgumentException.class)
+  public void testInvalidBoard_IllegalArgumentException() {
+    TicTacToeBoard board = new TicTacToeBoard("O..X.");
+  }
 
-  private boolean
+
   @Test
-  public void testValidBoardNoWinner() {
-    TicTacToeBoard board = new TicTacToeBoard("O...X.X..");
+  public void testInvalidBoard_MoveNumber() {
+    TicTacToeBoard board = new TicTacToeBoard("X.X.XO.XO");
+    assertEquals(Evaluation.UnreachableState, board.evaluate());
+  }
+
+  @Test
+  public void testInvalidBoard_DoubleWin() {
+    TicTacToeBoard board = new TicTacToeBoard(".OX.OX.OX");
+    assertEquals(Evaluation.UnreachableState, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardNoWinner_Unfinished() {
+    TicTacToeBoard board = new TicTacToeBoard("XOOOX..X.");
     assertEquals(Evaluation.NoWinner, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardNoWinner_Finished() {
+    TicTacToeBoard board = new TicTacToeBoard("XOOOXXXXO");
+    assertEquals(Evaluation.NoWinner, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardXWins_Vertical() {
+    TicTacToeBoard board = new TicTacToeBoard(".XOOX..X.");
+    assertEquals(Evaluation.Xwins, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardXWins_CaseSensitive() {
+    TicTacToeBoard board = new TicTacToeBoard("oOx.X.x..");
+    assertEquals(Evaluation.Xwins, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardOWins_Horizontal() {
+    TicTacToeBoard board = new TicTacToeBoard("x..OoOX..");
+    assertEquals(Evaluation.Owins, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardOWins_SpecialCharacters() {
+    TicTacToeBoard board = new TicTacToeBoard("?oXxO^xO7");
+    assertEquals(Evaluation.Owins, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardOWins_Diagonal() {
+    TicTacToeBoard board = new TicTacToeBoard("X.Oxo.O.x");
+    assertEquals(Evaluation.Owins, board.evaluate());
+  }
+
+  @Test
+  public void testValidBoardOWins_DoubleWin() {
+    TicTacToeBoard board = new TicTacToeBoard("OXoOoXOxX");
+    assertEquals(Evaluation.Owins, board.evaluate());
   }
 }
